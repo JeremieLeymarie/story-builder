b6d8b886ee094dff688d7b651815fe130c0b8221
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2024-11-11 12:52:12 +0100

    :lock: create token on auth endpoints

--- server/app/domains/user_service.py
+++ server/app/domains/user_service.py
@@ -1,8 +1,10 @@
+import os
 import bcrypt
 
 from repositories.user_repository_port import UserRepositoryPort
-from domains.type_def import FullUser, User
+from domains.type_def import AuthUser, FullUser
 from utils.errors import BadAuthException, InvalidActionException
+import jwt
 
 
 class UserService:
@@ -10,14 +12,14 @@ class UserService:
     def __init__(self, user_repository: UserRepositoryPort) -> None:
         self.user_repository = user_repository
 
-    def create(self, user: FullUser) -> User:
+    def create(self, user: FullUser) -> AuthUser:
         user_exists = self.user_repository.user_exists(
             username=user.username, email=user.email
         )
         if user_exists:
             raise InvalidActionException("Email or username is taken")
 
-        password_bytes = user.password.encode('utf-8')
+        password_bytes = user.password.encode("utf-8")
         hashed_bytes = bcrypt.hashpw(password_bytes, bcrypt.gensalt())
         hashed_password = hashed_bytes.decode("utf-8")
         created_user = self.user_repository.save(
@@ -29,9 +31,11 @@ class UserService:
             )
         )
 
-        return User.from_full_user(created_user)
+        token = self._generate_token(created_user)
 
-    def authentify(self, username_or_email: str, password: str) -> User:
+        return AuthUser.from_full_user(full_user=created_user, token=token)
+
+    def authentify(self, username_or_email: str, password: str) -> AuthUser:
         full_user = self.user_repository.get_by_username_or_email(
             username_or_email=username_or_email
         )
@@ -39,10 +43,16 @@ class UserService:
         if not full_user:
             raise BadAuthException("User not found")
 
-        password_bytes = password.encode('utf-8')
-        password_match = bcrypt.checkpw(password_bytes, full_user.password.encode('utf-8'))
+        password_bytes = password.encode("utf-8")
+        password_match = bcrypt.checkpw(
+            password_bytes, full_user.password.encode("utf-8")
+        )
 
         if not password_match:
             raise BadAuthException("Invalid password")
 
-        return User.from_full_user(full_user)
+        token = self._generate_token(full_user)
+        return AuthUser.from_full_user(full_user=full_user, token=token)
+
+    def _generate_token(self, user: FullUser) -> str:
+        return jwt.encode({"key": user.key}, os.getenv("JWT_SECRET"), algorithm="HS256")
