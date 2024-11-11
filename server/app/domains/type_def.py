9e80bd3220605eb86062cf169ff0f861e0bd3d2d
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2024-11-11 12:52:12 +0100

    :lock: create token on auth endpoints

--- server/app/domains/type_def.py
+++ server/app/domains/type_def.py
@@ -23,6 +23,22 @@ class User(BaseModel):
         )
 
 
+class AuthUser(BaseModel):
+    key: str
+    email: str
+    username: str
+    token: str
+
+    @classmethod
+    def from_full_user(cls, *, full_user: FullUser, token: str) -> Self:
+        return cls(
+            email=full_user.email,
+            username=full_user.username,
+            key=full_user.key,
+            token=token,
+        )
+
+
 class Action(BaseModel):
     text: str
     sceneKey: Optional[str] = None
