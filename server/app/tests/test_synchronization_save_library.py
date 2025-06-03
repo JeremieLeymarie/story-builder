1c2b17e1a0f93e1a25267b1f99dbc403bc790f19
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2025-06-03 15:19:28 +0200

    :white_check_mark: Synchronization - Story Progresses endpoints

--- server/app/tests/test_synchronization_save_library.py
+++ server/app/tests/test_synchronization_save_library.py
@@ -15,15 +15,15 @@ from utils.mongo.base_repository import (
 URL = "/api/save/library"
 
 
-def test_forbidden(api_test_infra_no_token) -> None:
-    response = api_test_infra_no_token.client.put(URL)
+def test_unauthorized(api_test_infra_no_auth) -> None:
+    response = api_test_infra_no_auth.client.put(URL)
 
     assert response.status_code == HTTPStatus.UNAUTHORIZED
     assert response.json() == {"detail": "Invalid token"}
 
 
 def test_no_rights_wrong_user_key(api_test_infra_authenticated) -> None:
-    client, auth_user, repo = api_test_infra_authenticated
+    client, repo, auth_user = api_test_infra_authenticated
 
     response = client.put(
         URL,
@@ -53,7 +53,7 @@ def test_no_rights_wrong_user_key(api_test_infra_authenticated) -> None:
 
 
 def test_with_builder_stories(api_test_infra_authenticated) -> None:
-    client, auth_user, repo = api_test_infra_authenticated
+    client, repo, auth_user = api_test_infra_authenticated
 
     response = client.put(
         URL,
@@ -85,7 +85,7 @@ def test_with_builder_stories(api_test_infra_authenticated) -> None:
 
 
 def test_empty_body(api_test_infra_authenticated) -> None:
-    client, auth_user, repo = api_test_infra_authenticated
+    client, repo, auth_user = api_test_infra_authenticated
 
     response = client.put(
         URL,
