67aba71694284e78d3bce111b85563c958351a56
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2025-06-10 11:51:51 +0200

    :arrow_up: Migrate to eslint v9

---
+++ client/eslint.config.ts
@@ -0,0 +1,67 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+import { fixupConfigRules } from "@eslint/compat";
+import tsParser from "@typescript-eslint/parser";
+import reactRefresh from "eslint-plugin-react-refresh";
+import js from "@eslint/js";
+
+import { FlatCompat } from "@eslint/eslintrc";
+
+const compat = new FlatCompat({
+  // @ts-expect-error TODO: Look into why bun types are not picked up here
+  baseDirectory: import.meta.dir,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  globalIgnores(["**/dist", "**/dev-dist"]),
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      parser: tsParser,
+    },
+
+    extends: fixupConfigRules(
+      compat.extends(
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:react-hooks/recommended",
+        "prettier",
+      ),
+    ),
+
+    plugins: {
+      "react-refresh": reactRefresh,
+    },
+
+    rules: {
+      "react-refresh/only-export-components": [
+        "warn",
+        {
+          allowConstantExport: true,
+        },
+      ],
+
+      eqeqeq: "error",
+
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          args: "all",
+          argsIgnorePattern: "^_",
+          caughtErrors: "all",
+          caughtErrorsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+
+      "no-unused-vars": "off",
+    },
+  },
+]);
