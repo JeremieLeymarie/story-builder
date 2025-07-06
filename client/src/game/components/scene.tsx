ad9dad49b7196237e5737d0343b6547191dc220b
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2025-07-06 18:42:05 +0200

    :iphone: Game - Make scenes & actions more responsive on mobile

--- client/src/game/components/scene.tsx
+++ client/src/game/components/scene.tsx
@@ -18,14 +18,16 @@ export const GameScene = ({
   isLastScene,
 }: GameSceneProps) => {
   return (
-    <div className="flex w-full justify-center py-16">
-      <div className="w-8/12">
+    <div className="flex w-full justify-center py-8">
+      <div className="w-11/12 lg:w-8/12">
         <div className="w-full px-6 py-8">
           <div>
             <h1 className="scroll-m-20 text-3xl font-bold tracking-tight lg:text-4xl">
               {title}
             </h1>
-            <p className="leading-7 not-first:mt-6">{content}</p>
+            <p className="leading-7 wrap-break-word not-first:mt-6">
+              {content}
+            </p>
             <div className="mt-4 flex w-full flex-wrap gap-3">
               {actions.map((action) => (
                 <SceneAction
@@ -40,8 +42,8 @@ export const GameScene = ({
         </div>
         {isLastScene && (
           <div className="mt-6 flex flex-col items-center gap-4">
-            <Divider className="w-[200px]" />
-            <p className="text-muted-foreground w-6/12 text-center italic">
+            <Divider className="w-4/12" />
+            <p className="text-muted-foreground w-10/12 text-center italic lg:w-6/12">
               This was the last scene of this story... Yes, we know, endings are
               always difficult. But you can still go back to your library and
               find more stories to play!
