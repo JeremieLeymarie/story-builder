ac2bd3fb0df38e339a78cb3bc4d5377eedcece4f
Guillaum-th <guillaume.thiebaud@epitech.eu>
2025-07-07 16:37:40 +0200

    :construction: add editor

---
+++ client/src/components/blocks/editor-00/plugins.tsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { LexicalErrorBoundary } from "@lexical/react/LexicalErrorBoundary";
+import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
+
+import { ContentEditable } from "@/components/editor/editor-ui/content-editable";
+import { ToolbarPlugin } from "@/components/editor/plugins/toolbar/toolbar-plugin";
+import { FontFormatToolbarPlugin } from "@/components/editor/plugins/toolbar/font-format-toolbar-plugin";
+
+export function Plugins() {
+  const [floatingAnchorElem, setFloatingAnchorElem] =
+    useState<HTMLDivElement | null>(null);
+
+  const onRef = (_floatingAnchorElem: HTMLDivElement) => {
+    if (_floatingAnchorElem !== null) {
+      setFloatingAnchorElem(_floatingAnchorElem);
+    }
+  };
+
+  return (
+    <div className="relative">
+      <ToolbarPlugin>
+        {() => (
+          <div className="vertical-align-middle sticky top-0 z-10 flex gap-2 overflow-auto border-b p-1">
+            <FontFormatToolbarPlugin format="bold" />
+            <FontFormatToolbarPlugin format="italic" />
+            <FontFormatToolbarPlugin format="underline" />
+            <FontFormatToolbarPlugin format="strikethrough" />
+          </div>
+        )}
+      </ToolbarPlugin>
+      <div className="relative">
+        <RichTextPlugin
+          contentEditable={
+            <div className="">
+              <div className="" ref={onRef}>
+                <ContentEditable placeholder={"Start typing ..."} />
+              </div>
+            </div>
+          }
+          ErrorBoundary={LexicalErrorBoundary}
+        />
+        {/* editor plugins */}
+      </div>
+      {/* actions plugins */}
+    </div>
+  );
+}
