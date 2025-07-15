a4da667c3d59791fab1804218d499ed6297e4b73
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2025-07-15 19:33:32 +0200

    :art: Clean

---
+++ client/src/builder/components/scene-editor/hooks/use-scene-editor-form.ts
@@ -0,0 +1,55 @@
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useEffect } from "react";
+import { SceneSchema, sceneSchema, SceneUpdatePayload } from "../schema";
+import { useDebouncer } from "@tanstack/react-pacer/debouncer";
+
+export const useSceneEditorForm = ({
+  scene,
+  onSave,
+}: {
+  scene: SceneUpdatePayload;
+  onSave: (scene: SceneUpdatePayload) => void;
+}) => {
+  const form = useForm<SceneSchema>({
+    resolver: zodResolver(sceneSchema),
+    defaultValues: {
+      content: scene?.content,
+      title: scene?.title,
+      actions: scene?.actions,
+    },
+  });
+
+  const debouncer = useDebouncer(
+    (scene) => {
+      form.handleSubmit((values: SceneSchema) => {
+        onSave({
+          key: scene.key,
+          storyKey: scene.storyKey,
+          actions: values.actions,
+          content: values.content,
+          title: values.title,
+        });
+      })();
+    },
+    { wait: 500 },
+    () => {}, // Never re-render when internal debouncer state changes
+  );
+
+  useEffect(() => {
+    // Update the form when the default values change, which are 'cached' otherwise
+    if (scene) form.reset(scene);
+  }, [scene, form, debouncer]);
+
+  useEffect(() => {
+    const callback = form.subscribe({
+      formState: {
+        values: true,
+      },
+      callback: () => debouncer.maybeExecute(scene),
+    });
+    return () => callback();
+  }, [debouncer, form, scene]);
+
+  return form;
+};
