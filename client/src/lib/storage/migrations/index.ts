6aa4243174455c49e1cf9b2661262b6a675b3a9e
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2025-07-21 17:11:09 +0200

    :alembic: Migration system

---
+++ client/src/lib/storage/migrations/index.ts
@@ -0,0 +1,27 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type MigrateFunction = (...params: any[]) => Promise<void>;
+
+export type Migration = {
+  key: string;
+  migrate: MigrateFunction;
+};
+
+// The order is important here
+export const MIGRATIONS: Migration[] = [];
+
+const LS_KEY = "migrations";
+
+export const executeMigrations = async () => {
+  const executedMigrations: Record<string, boolean> = JSON.parse(
+    localStorage.getItem(LS_KEY) ?? "{}",
+  );
+
+  for (let i = 0; i < MIGRATIONS.length; i++) {
+    const migration = MIGRATIONS[i]!;
+    if (migration.key in executedMigrations) continue;
+
+    await migration.migrate();
+    executedMigrations[migration.key] = true;
+    localStorage.setItem(LS_KEY, JSON.stringify(executedMigrations));
+  }
+};
