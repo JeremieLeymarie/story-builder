2d694455daacf7079d681e44411f68a10aed430f
francisvirginius <francisvirginius@gmail.com>
2025-08-19 14:52:52 +0200

    Btn delete added

--- client/src/domains/game/library-service.ts
+++ client/src/domains/game/library-service.ts
@@ -215,6 +215,10 @@ export const _getLibraryService = ({
       );
     },
 
+    deleteStoryProgress: async (progressKey: string) => {
+      await localRepository.deleteStoryProgresses([progressKey]);
+    },
+
     loadLibraryState: async ({
       progresses,
       libraryStories,
