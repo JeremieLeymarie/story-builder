d5b16672e000782aa3ccfdcc23b1719cfa9944f3
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2025-08-25 10:29:43 +0200

    :fire: Use existing logo & remove duplicate

--- client/src/wikis/article.tsx
+++ client/src/wikis/article.tsx
@@ -1,11 +1,10 @@
-import { Logo } from "@/design-system/components/logo";
 import { Button } from "@/design-system/primitives";
 
 const EmptyState = () => {
   return (
     <div className="flex h-full w-full items-center justify-center">
       <div className="bg-muted flex h-[300px] w-[400px] flex-col items-center justify-center space-y-2 rounded shadow">
-        <Logo width={150} height={150} />
+        <img src="/story-builder-logo.svg" width={150} height={150} />
         <p className="text-muted-foreground italic">This wiki is empty...</p>
         <Button>Create your first article</Button>
       </div>
