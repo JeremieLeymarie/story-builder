d5c2678a4c8e972956421ed66180d7ef5b53add8
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2025-08-26 16:34:39 +0200

    :art: Clean

--- client/src/builder/components/story-form-dialog.tsx
+++ client/src/builder/components/story-form-dialog.tsx
@@ -1,27 +1,16 @@
 import {
-  Button,
   Dialog,
   DialogContent,
   DialogDescription,
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-  Input,
-  Textarea,
 } from "@/design-system/primitives";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { ReactNode, useState } from "react";
-import { useForm } from "react-hook-form";
-import { storySchema, StorySchema } from "./story-schema";
-import { StoryGenreCombobox } from "@/design-system/components/story-genre-combobox";
+import { StorySchema } from "./story-schema";
 import { CreateStoryPayload } from "../hooks/use-builder-stories";
+import { StoryForm } from "./story-form";
+import { useStoryForm } from "../hooks/use-story-form";
 
 type StoryFormProps = {
   onSubmit: (props: CreateStoryPayload) => void;
@@ -48,14 +37,7 @@ export const ControlledStoryFormDialog = ({
   title,
   description,
 }: StoryFormProps & { open: boolean; setOpen: (open: boolean) => void }) => {
-  const form = useForm<StorySchema>({
-    resolver: zodResolver(storySchema),
-    defaultValues: defaultValues ?? {
-      title: "",
-      description: "",
-      image: "",
-    },
-  });
+  const form = useStoryForm({ defaultValues });
 
   const handleOpen = (open: boolean) => {
     setOpen(open);
@@ -75,81 +57,7 @@ export const ControlledStoryFormDialog = ({
           <DialogTitle>{title}</DialogTitle>
           <DialogDescription>{description}</DialogDescription>
         </DialogHeader>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(submit)} className="space-y-4">
-            <FormField
-              control={form.control}
-              name="title"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Title</FormLabel>
-                  <FormControl>
-                    <Input
-                      placeholder="The Great Journey To The Green River"
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormDescription>
-                    The displayed title of your story
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="genres"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Genre</FormLabel>
-                  <FormControl>
-                    <StoryGenreCombobox
-                      onChange={field.onChange}
-                      values={field.value}
-                    />
-                  </FormControl>
-                  <FormDescription>The genre(s) of your story</FormDescription>
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="description"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Description</FormLabel>
-                  <FormControl>
-                    <Textarea
-                      placeholder="A wonderful epic tale through the world of Penthetir. "
-                      {...field}
-                    />
-                  </FormControl>
-                  <FormDescription>
-                    A short description of your story
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name="image"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Image</FormLabel>
-                  <FormControl>
-                    <Input placeholder="http://your-image-url.com" {...field} />
-                  </FormControl>
-                  <FormDescription>
-                    The cover image for your story
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit">Save</Button>
-          </form>
-        </Form>
+        <StoryForm form={form} onSubmit={submit} />
       </DialogContent>
     </Dialog>
   );
