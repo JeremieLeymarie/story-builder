00056a64fb69dedf9ff83b2ab44d95b6dd52cdbd
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2025-08-30 16:11:31 +0200

    :recycle: Builder -Refacto use-builder-editor after feedback

--- client/src/builder/components/builder-editor-bar/editor-bar.tsx
+++ client/src/builder/components/builder-editor-bar/editor-bar.tsx
@@ -6,20 +6,21 @@ import { Toolbar, ToolbarTitle } from "@/design-system/components/toolbar";
 import { StoryEditor } from "./story-editor/story-editor";
 
 export const EditorBar = () => {
-  // Here TS discriminated union won't work if we don't select the whole state
-  const state = useBuilderEditorStore();
+  const currentEditor = useBuilderEditorStore((state) => state.editor);
 
-  if (!state.type) return null;
+  if (!currentEditor) return null;
 
   return (
-    <Toolbar className={cn("min-w-[450px]", state.type === null && "hidden")}>
+    <Toolbar
+      className={cn("min-w-[450px]", currentEditor.type === null && "hidden")}
+    >
       <ToolbarTitle className="mb-2">
-        {match(state)
+        {match(currentEditor)
           .with({ type: "scene-editor" }, () => "Edit scene")
           .with({ type: "story-editor" }, () => "Edit story")
           .exhaustive()}
       </ToolbarTitle>
-      {match(state)
+      {match(currentEditor)
         .with({ type: "scene-editor" }, ({ payload }) => (
           <SceneForm
             isFirstScene={payload.isFirstScene}
