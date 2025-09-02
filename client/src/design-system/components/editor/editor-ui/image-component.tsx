68244ebdfdf57cfe085cbddddd35f2d01b4d4bc1
Guillaum-th <guillaume.thiebaud@epitech.eu>
2025-09-02 14:53:32 +0200

    Exhaustive-deps rules doesn't work

--- client/src/design-system/components/editor/editor-ui/image-component.tsx
+++ client/src/design-system/components/editor/editor-ui/image-component.tsx
@@ -1,3 +1,6 @@
+// Exhaustive-deps rules doesn't work well with the compiler, see: https://github.com/reactwg/react-compiler/discussions/18
+// eslint-disable react-hooks/exhaustive-deps
+
 import { Suspense, useEffect, useRef, useState } from "react";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { useLexicalEditable } from "@lexical/react/useLexicalEditable";
@@ -28,9 +31,6 @@ import {
 import { ImageResizer } from "@/design-system/components/editor/editor-ui/image-resizer";
 import { $isImageNode } from "@/design-system/components/editor/nodes/image-node";
 import { SimpleLoader } from "../../simple-loader";
-
-// Exhaustive-deps rules doesn't work well with the compiler, see: https://github.com/reactwg/react-compiler/discussions/18
-// eslint-disable-next-line react-hooks/exhaustive-deps
 
 export const RIGHT_CLICK_IMAGE_COMMAND: LexicalCommand<MouseEvent> =
   createCommand("RIGHT_CLICK_IMAGE_COMMAND");
