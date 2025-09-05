dcc6f3965674381359eac9040c371f4d66fe953e
PicoLeaf <85674246+PicoLeaf@users.noreply.github.com>
2025-09-05 11:49:33 +0200

    :recycle: split logic

--- client/src/builder/hooks/use-add-scenes.ts
+++ client/src/builder/hooks/use-add-scenes.ts
@@ -39,6 +39,7 @@ export const useAddScenes = () => {
       if (!reactFlowRef.current) return { x: 0, y: 0 };
 
       const rect = reactFlowRef.current.getBoundingClientRect();
+      // find the center
       const position = {
         x: rect.x + rect.width / 2,
         y: rect.y + rect.height / 2,
@@ -73,68 +74,69 @@ export const useAddScenes = () => {
     if (Number.isFinite(topY)) position.y -= topY;
   };
 
+  const addScenesToDB = (newScenes: NewScene[], position: XYPosition) => {
+    // promote the new scenes into future resident of the database
+    const scenes = structuredClone(newScenes) as Scene[];
+    const keys = scenes.map(() => nanoid());
+    scenes.forEach((scene, i) => {
+      scene.key = keys[i]!;
+      scene.storyKey = story.key;
+      // relative position to absolute position
+      scene.builderParams.position = {
+        x: scene.builderParams.position.x + position.x,
+        y: scene.builderParams.position.y + position.y,
+      };
+      // in case the sceneKeys were defined relative to other scenes in the batch,
+      // promote the relative indicies to absolute keys
+      scene.actions.forEach((action) => {
+        if (action.sceneKey && action.sceneKey[0] === "=") {
+          const recipient = parseInt(action.sceneKey.slice(1));
+          if (Number.isSafeInteger(recipient) && recipient < keys.length)
+            action.sceneKey = keys[recipient];
+        }
+      });
+    });
+    // the scenes move to their new homes
+    builderService.bulkUpdateScenes({ scenes });
+
+    return scenes;
+  };
+
+  const updateFrontend = (scenes: Scene[]) => {
+    const [nodes, edges] = scenesToNodesAndEdgesAdapter({ scenes, story });
+    unselectNodesAndEdges();
+    nodes.forEach((node) => {
+      node.selected = true;
+    });
+    addNodes(nodes);
+    addEdges(edges);
+
+    if (scenes.length === 1) {
+      const scene = scenes[0]!;
+      openSceneEditor({
+        type: "scene-editor",
+        payload: {
+          scene,
+          isFirstScene: story.firstSceneKey === scene.key,
+        },
+      });
+    } else {
+      // FIXME: This triggers *before* the scenes are added to the dom
+      // This is a problem because XYDrag expects the nodes to instantiated to listen for mouse events
+      // As such it is not yet possible to immediatly drag newly added nodes
+      setState({ nodesSelectionActive: true });
+    }
+  };
+
   const addScenes = (
     newScenes: NewScene[],
     position: XYPosition = getInitialPlacement(),
   ): Scene[] | null => {
     if (!newScenes.length) return [];
     computeEfficientPlacement(newScenes, position);
-
     try {
-      // promote the new scenes into future resident of the database
-      const scenes = structuredClone(newScenes) as Scene[];
-      const keys = scenes.map(() => nanoid());
-      scenes.forEach((scene, i) => {
-        scene.key = keys[i]!;
-        scene.storyKey = story.key;
-        // relative position to absolute position
-        scene.builderParams.position = {
-          x: scene.builderParams.position.x + position.x,
-          y: scene.builderParams.position.y + position.y,
-        };
-        // in case the sceneKeys were defined relative to other scenes in the batch,
-        // promote the relative indicies to absolute keys
-        scene.actions.forEach((action) => {
-          if (action.sceneKey && action.sceneKey[0] === "=") {
-            const sceneKey = parseInt(action.sceneKey.slice(1));
-            if (Number.isSafeInteger(sceneKey) && sceneKey < keys.length)
-              action.sceneKey = keys[sceneKey];
-          }
-        });
-        // FIXME: self referential nodes aren't resolved
-      });
-
-      // update reactflow
-      const [newNodes, newEdges] = scenesToNodesAndEdgesAdapter({
-        scenes,
-        story,
-      });
-      newNodes.forEach((node) => {
-        node.selected = true;
-      });
-      unselectNodesAndEdges();
-      addNodes(newNodes);
-      addEdges(newEdges);
-
-      if (scenes.length === 1) {
-        const scene = scenes[0]!;
-        openSceneEditor({
-          type: "scene-editor",
-          payload: {
-            scene,
-            isFirstScene: story.firstSceneKey === scene.key,
-          },
-        });
-      } else {
-        // FIXME: This triggers *before* the scenes are added to the dom
-        // This is a problem because XYDrag expects the nodes to instantiated to listen for mouse events
-        // As such it is not yet possible to immediatly drag newly added nodes
-        setState({ nodesSelectionActive: true });
-      }
-
-      // the scenes move to their new homes
-      builderService.bulkUpdateScenes({ scenes });
-
+      const scenes = addScenesToDB(newScenes, position);
+      updateFrontend(scenes);
       return scenes;
     } catch (err) {
       handleError(err);
