4c0069ce66b46338cfa718e07f377f3fcbbb16da
Guillaum-th <guillaume.thiebaud@epitech.eu>
2025-09-18 11:27:57 +0200

    add helper

--- client/src/design-system/components/editor/editor-ui/image-component.tsx
+++ client/src/design-system/components/editor/editor-ui/image-component.tsx
@@ -1,37 +1,11 @@
-import { Suspense, useEffect, useRef, useState } from "react";
+import { Suspense, useRef, useState } from "react";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
-import { useLexicalEditable } from "@lexical/react/useLexicalEditable";
-import { useLexicalNodeSelection } from "@lexical/react/useLexicalNodeSelection";
-import { mergeRegister } from "@lexical/utils";
-import type {
-  BaseSelection,
-  LexicalCommand,
-  LexicalEditor,
-  NodeKey,
-} from "lexical";
-import {
-  $getNodeByKey,
-  $getSelection,
-  $isNodeSelection,
-  $setSelection,
-  CLICK_COMMAND,
-  COMMAND_PRIORITY_LOW,
-  createCommand,
-  DRAGSTART_COMMAND,
-  KEY_BACKSPACE_COMMAND,
-  KEY_DELETE_COMMAND,
-  KEY_ENTER_COMMAND,
-  KEY_ESCAPE_COMMAND,
-  SELECTION_CHANGE_COMMAND,
-} from "lexical";
+import type { NodeKey } from "lexical";
+import { $isNodeSelection } from "lexical";
 
 import { ImageResizer } from "@/design-system/components/editor/editor-ui/image-resizer";
-import { $isImageNode } from "@/design-system/components/editor/nodes/image-node";
 import { SimpleLoader } from "../../simple-loader";
-
-// eslint-disable-next-line react-refresh/only-export-components
-export const RIGHT_CLICK_IMAGE_COMMAND: LexicalCommand<MouseEvent> =
-  createCommand("RIGHT_CLICK_IMAGE_COMMAND");
+import { useImageCommands } from "../helpers/use-image-commands";
 
 const LazyImage = ({
   altText,
@@ -102,182 +76,10 @@ const ImageComponent = ({
 }) => {
   const imageRef = useRef<null | HTMLImageElement>(null);
   const buttonRef = useRef<HTMLButtonElement | null>(null);
-  const [isSelected, setSelected, clearSelection] =
-    useLexicalNodeSelection(nodeKey);
-  const [isResizing, setIsResizing] = useState<boolean>(false);
   const [editor] = useLexicalComposerContext();
-  const [selection, setSelection] = useState<BaseSelection | null>(null);
-  const activeEditorRef = useRef<LexicalEditor | null>(null);
   const [isLoadError, setIsLoadError] = useState<boolean>(false);
-  const isEditable = useLexicalEditable();
-
-  // Exhaustive-deps rules doesn't work well with the compiler, see: https://github.com/reactwg/react-compiler/discussions/18
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const $onDelete = (event: KeyboardEvent) => {
-    const deleteSelection = $getSelection();
-    if (isSelected && $isNodeSelection(deleteSelection)) {
-      event.preventDefault();
-      editor.update(() => {
-        deleteSelection.getNodes().forEach((node) => {
-          if ($isImageNode(node)) {
-            node.remove();
-          }
-        });
-      });
-    }
-    return false;
-  };
-
-  // Exhaustive-deps rules doesn't work well with the compiler, see: https://github.com/reactwg/react-compiler/discussions/18
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const $onEnter = (event: KeyboardEvent) => {
-    const latestSelection = $getSelection();
-    const buttonElem = buttonRef.current;
-    if (
-      isSelected &&
-      $isNodeSelection(latestSelection) &&
-      latestSelection.getNodes().length === 1
-    ) {
-      if (buttonElem !== null && buttonElem !== document.activeElement) {
-        event.preventDefault();
-        buttonElem.focus();
-        return true;
-      }
-    }
-    return false;
-  };
-
-  // Exhaustive-deps rules doesn't work well with the compiler, see: https://github.com/reactwg/react-compiler/discussions/18
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const $onEscape = (event: KeyboardEvent) => {
-    if (buttonRef.current === event.target) {
-      $setSelection(null);
-      editor.update(() => {
-        setSelected(true);
-        const parentRootElement = editor.getRootElement();
-        if (parentRootElement !== null) {
-          parentRootElement.focus();
-        }
-      });
-      return true;
-    }
-    return false;
-  };
-
-  // Exhaustive-deps rules doesn't work well with the compiler, see: https://github.com/reactwg/react-compiler/discussions/18
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const onClick = (payload: MouseEvent) => {
-    const event = payload;
-
-    if (isResizing) {
-      return true;
-    }
-    if (event.target === imageRef.current) {
-      if (event.shiftKey) {
-        setSelected(!isSelected);
-      } else {
-        clearSelection();
-        setSelected(true);
-      }
-      return true;
-    }
-
-    return false;
-  };
-
-  useEffect(() => {
-    const unregister = mergeRegister(
-      editor.registerUpdateListener(({ editorState }) => {
-        setSelection(editorState.read(() => $getSelection()));
-      }),
-      editor.registerCommand(
-        SELECTION_CHANGE_COMMAND,
-        (_, activeEditor) => {
-          activeEditorRef.current = activeEditor;
-          return false;
-        },
-        COMMAND_PRIORITY_LOW,
-      ),
-      editor.registerCommand<MouseEvent>(
-        CLICK_COMMAND,
-        onClick,
-        COMMAND_PRIORITY_LOW,
-      ),
-      editor.registerCommand<MouseEvent>(
-        RIGHT_CLICK_IMAGE_COMMAND,
-        onClick,
-        COMMAND_PRIORITY_LOW,
-      ),
-      editor.registerCommand(
-        DRAGSTART_COMMAND,
-        (event) => {
-          if (event.target === imageRef.current) {
-            // TODO This is just a temporary workaround for FF to behave like other browsers.
-            // Ideally, this handles drag & drop too (and all browsers).
-            event.preventDefault();
-            return true;
-          }
-          return false;
-        },
-        COMMAND_PRIORITY_LOW,
-      ),
-      editor.registerCommand(
-        KEY_DELETE_COMMAND,
-        $onDelete,
-        COMMAND_PRIORITY_LOW,
-      ),
-      editor.registerCommand(
-        KEY_BACKSPACE_COMMAND,
-        $onDelete,
-        COMMAND_PRIORITY_LOW,
-      ),
-      editor.registerCommand(KEY_ENTER_COMMAND, $onEnter, COMMAND_PRIORITY_LOW),
-      editor.registerCommand(
-        KEY_ESCAPE_COMMAND,
-        $onEscape,
-        COMMAND_PRIORITY_LOW,
-      ),
-    );
-
-    return () => {
-      unregister();
-    };
-  }, [
-    clearSelection,
-    editor,
-    isResizing,
-    isSelected,
-    nodeKey,
-    $onDelete,
-    $onEnter,
-    $onEscape,
-    onClick,
-    setSelected,
-  ]);
-
-  const onResizeEnd = (
-    nextWidth: "inherit" | number,
-    nextHeight: "inherit" | number,
-  ) => {
-    // Delay hiding the resize bars for click case
-    setTimeout(() => {
-      setIsResizing(false);
-    }, 200);
-
-    editor.update(() => {
-      const node = $getNodeByKey(nodeKey);
-      if ($isImageNode(node)) {
-        node.setWidthAndHeight(nextWidth, nextHeight);
-      }
-    });
-  };
-
-  const onResizeStart = () => {
-    setIsResizing(true);
-  };
-
-  const draggable = isSelected && $isNodeSelection(selection) && !isResizing;
-  const isFocused = (isSelected || isResizing) && isEditable;
+  const { draggable, isFocused, selection, onResizeEnd, onResizeStart } =
+    useImageCommands({ nodeKey, imageRef });
   return (
     <Suspense fallback={<SimpleLoader />}>
       <span className="user-select-none editor-image relative inline-block cursor-default">
