d66cc399f320f5246da82c581cfc030da68f9646
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2025-10-31 15:57:50 +0100

    :construction: First iteration of Wiki Node & Plugin in rich text editor

--- client/src/design-system/components/editor/nodes.ts
+++ client/src/design-system/components/editor/nodes.ts
@@ -7,6 +7,7 @@ import {
   TextNode,
 } from "lexical";
 import { ImageNode } from "./nodes/image-node";
+import { WikiNode } from "./nodes/wiki-node";
 
 export const nodes: ReadonlyArray<Klass<LexicalNode> | LexicalNodeReplacement> =
-  [HeadingNode, ParagraphNode, TextNode, QuoteNode, ImageNode];
+  [HeadingNode, ParagraphNode, TextNode, QuoteNode, ImageNode, WikiNode];
