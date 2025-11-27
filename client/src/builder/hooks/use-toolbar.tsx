79c8e99130dfd43eaced1f4e6c1d07c0d02e63fe
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2025-11-27 16:34:23 +0100

    :lipstick: Expand/Minify Builder toolbar

--- client/src/builder/hooks/use-toolbar.tsx
+++ client/src/builder/hooks/use-toolbar.tsx
@@ -1,27 +1,36 @@
 import { useTestStory } from "./use-test-story";
 import { useRouter } from "@tanstack/react-router";
 import { getBuilderService } from "@/get-builder-service";
+import { useBuilderContext } from "./use-builder-context";
+import { useAddScene } from "./use-add-scene";
+import { useBuilderEditorStore } from "./use-scene-editor-store";
+import { useState } from "react";
 
-type ToolbarProps = {
-  storyKey: string;
-  remoteStoryKey?: string;
-};
-
-export const useToolbar = ({ storyKey }: ToolbarProps) => {
+export const useToolbarActions = () => {
   const { testStory } = useTestStory();
   const { navigate } = useRouter();
+  const { story } = useBuilderContext();
+  const { addScene } = useAddScene();
+  const openBuilderEditor = useBuilderEditorStore((state) => state.open);
 
   const deleteStory = async () => {
-    await getBuilderService().deleteStory(storyKey);
-    navigate({ to: "/builder/stories" });
-  };
-
-  const openUIEditor = () => {
+    await getBuilderService().deleteStory(story.key);
     navigate({ to: "/builder/stories" });
   };
 
   return {
     testStory,
     deleteStory,
+    addScene,
+    openBuilderEditor,
+  };
+};
+
+export const useToolbar = () => {
+  const [isExpanded, setIsExpanded] = useState(true);
+
+  return {
+    isExpanded,
+    toggleExpanded: () => setIsExpanded((prev) => !prev),
   };
 };
