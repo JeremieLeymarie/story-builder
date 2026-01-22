62e9aaa50f396ad669bec1889e136dd1c5014fe9
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2026-01-22 11:59:58 +0100

    :wastebasket: Remove legacy migration system

--- client/src/lib/storage/migrations/types.ts
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type MigrateFunction = (...params: any[]) => Promise<void>;
-
-export type Migration = {
-  key: string;
-  migrate: MigrateFunction;
-};
