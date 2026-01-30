2d950546d1676e85f0b43df0efce25c3d2aee6e3
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2026-01-30 15:06:12 +0100

    :children_crossing: Make probability editable on edges

--- client/src/builder/components/edges/custom-edge.tsx
+++ client/src/builder/components/edges/custom-edge.tsx
@@ -1,10 +1,27 @@
 import { BuilderEdge } from "@/builder/types";
+import { Input } from "@/design-system/primitives";
 import {
   getBezierPath,
   EdgeLabelRenderer,
   BaseEdge,
   type EdgeProps,
 } from "@xyflow/react";
+import { useState } from "react";
+import { useMaskito } from "@maskito/react";
+import { MaskitoOptions } from "@maskito/core";
+import {
+  maskitoCaretGuard,
+  maskitoPostfixPostprocessorGenerator,
+} from "@maskito/kit";
+
+const percentMask = {
+  mask: /([0-9]{0,3}\.?[0-9]{0,2})/,
+  postprocessors: [maskitoPostfixPostprocessorGenerator("%")],
+  plugins: [
+    // First item = min index for caret, last item = max index
+    maskitoCaretGuard((value) => [0, value.length - 1]),
+  ],
+} satisfies MaskitoOptions;
 
 const CustomEdge = ({
   id,
@@ -14,6 +31,8 @@ const CustomEdge = ({
   targetY,
   sourcePosition,
   targetPosition,
+  markerEnd,
+  style,
   data,
 }: EdgeProps<BuilderEdge>) => {
   const [edgePath, labelX, labelY] = getBezierPath({
@@ -24,19 +43,35 @@ const CustomEdge = ({
     targetY,
     targetPosition,
   });
+  const [isFocused, setIsFocused] = useState(false);
+  const inputRef = useMaskito({ options: percentMask });
 
   return (
     <>
-      <BaseEdge id={id} path={edgePath} />
+      <BaseEdge id={id} path={edgePath} style={style} markerEnd={markerEnd} />
       {data?.hasSiblings && (
         <EdgeLabelRenderer>
           <div
+            onClick={() => {
+              setIsFocused(true);
+            }}
             style={{
               transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
+              pointerEvents: "all", // does not exist in tailwind
             }}
-            className="nodrag nopan absolute z-10 rounded border bg-white px-3 py-2"
+            className="nodrag nopan absolute z-10 flex h-10 w-16 origin-center cursor-pointer items-center justify-center rounded border bg-white text-base"
           >
-            {data.probability}%
+            {isFocused ? (
+              <Input
+                autoFocus
+                defaultValue={`${data.probability}%`}
+                className="h-full border-none px-2 text-center md:text-base"
+                onBlur={() => setIsFocused(false)}
+                ref={inputRef}
+              />
+            ) : (
+              <span>{data.probability}%</span>
+            )}
           </div>
         </EdgeLabelRenderer>
       )}
