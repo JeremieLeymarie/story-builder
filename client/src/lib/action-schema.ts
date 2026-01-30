cae8179f44ffbe4fc79474d63b3fe1f9be446671
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2026-01-30 16:46:03 +0100

    :safety_vest: Validate random events in JSON import

--- client/src/lib/action-schema.ts
+++ client/src/lib/action-schema.ts
@@ -3,7 +3,16 @@ import z from "zod";
 const baseActionSchema = z.object({
   key: z.nanoid(),
   text: z.string({ message: "Text is required" }),
-  targets: z.array(z.object({ sceneKey: z.nanoid(), probability: z.number() })),
+  targets: z
+    .array(z.object({ sceneKey: z.nanoid(), probability: z.number() }))
+    .refine((values) => {
+      const totalProbabilities = values.reduce(
+        (acc, v) => acc + v.probability,
+        0,
+      );
+
+      return totalProbabilities > 0;
+    }),
 });
 
 export const actionSchema = z.discriminatedUnion("type", [
