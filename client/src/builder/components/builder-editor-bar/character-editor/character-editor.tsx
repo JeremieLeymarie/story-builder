775d471505dfbae923a9eb906576e12a0989e857
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2026-02-20 10:54:32 +0100

    :construction: Get character config hook

--- client/src/builder/components/builder-editor-bar/character-editor/character-editor.tsx
+++ client/src/builder/components/builder-editor-bar/character-editor/character-editor.tsx
@@ -4,14 +4,22 @@ import {
   ToolbarTitle,
 } from "@/design-system/components/toolbar";
 import { EditCharacterForm } from "./edit-character-form";
+import { useGetCharacterConfig } from "@/builder/hooks/use-get-character-config";
+import { SimpleLoader } from "@/design-system/components/simple-loader";
 
 export const CharacterEditor = () => {
+  const { characterConfig, isLoading } = useGetCharacterConfig();
+
   return (
     <Toolbar className="w-80">
       <ToolbarHeader className="flex-row items-center justify-between">
         <ToolbarTitle>Edit character</ToolbarTitle>
       </ToolbarHeader>
-      <EditCharacterForm />
+      {!characterConfig || isLoading ? (
+        <SimpleLoader className="m-auto mt-4" />
+      ) : (
+        <EditCharacterForm />
+      )}
     </Toolbar>
   );
 };
