734cae95ce59bafc26d4ae13ab0a2ac3b27b2a50
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2026-03-13 11:21:06 +0100

    :art: Clean

--- client/src/game/components/character-card.tsx
+++ client/src/game/components/character-card.tsx
@@ -12,7 +12,6 @@ import {
   BookUserIcon,
 } from "lucide-react";
 import { useLocalGameSettings } from "../hooks/use-local-game-settings";
-import { cn } from "@/lib/style";
 
 export const CharacterCard = ({
   character,
@@ -27,7 +26,7 @@ export const CharacterCard = ({
   if (!shouldDisplayCharacter) return null;
 
   return (
-    <Card className={cn("gap-1 py-2 transition-all duration-1000")}>
+    <Card className="gap-1 py-2">
       <CardHeader className="flex items-center justify-between">
         <CardTitle className="flex items-center gap-1">
           <BookUserIcon size={18} /> Your character
@@ -50,7 +49,7 @@ export const CharacterCard = ({
       </CardHeader>
       {/* TODO: animate expand/contract */}
       {isCharacterCardOpen && (
-        <CardContent className="flex flex-wrap gap-1 transition-all transition-discrete duration-1000">
+        <CardContent className="flex flex-wrap gap-1">
           {Object.values(character!.attributes).map(
             ({ key, name, value }, i) => (
               <div key={key}>
