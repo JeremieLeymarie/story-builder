8c2545ba8f96b0e5eaed519d734d1e1bc58d5cd3
Louis Salanoubat <louis.salanoubat@epitech.eu>
2026-04-21 15:22:31 +0200

    :iphone: Make scene and story editors responsive on mobile
    NB: branch was initially created from 476-renommer-histoire-import by mistake, now rebased on develop with only the responsive commit.

--- client/src/design-system/components/editor/plugins/editor-plugins.tsx
+++ client/src/design-system/components/editor/plugins/editor-plugins.tsx
@@ -12,7 +12,7 @@ export const EditorPlugins = ({
 }) => {
   return (
     <>
-      <div className="vertical-align-middle border-tl sticky top-0 z-10 flex items-center gap-1 overflow-auto rounded-t-lg border-b bg-white p-1">
+      <div className="vertical-align-middle border-tl sticky top-0 z-10 flex w-full min-w-0 items-center gap-1 overflow-x-auto rounded-t-lg border-b bg-white p-1 [&>*]:shrink-0">
         <FontFormatToolbarPlugin format="bold" />
         <FontFormatToolbarPlugin format="italic" />
         <FontFormatToolbarPlugin format="underline" />
