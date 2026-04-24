5e4ef5bc676be5f8611978eb9ec37657dd984bbb
JeremieLeymarie <jeremie.leymarie@epitech.eu>
2026-04-24 12:01:49 +0200

    :sparkles: Fully functional wiki search

--- client/src/wikis/wiki-search.tsx
+++ client/src/wikis/wiki-search.tsx
@@ -14,19 +14,26 @@ import { useState } from "react";
 import { RichText } from "@/design-system/components/editor/components/rich-text-editor";
 import { EditorContextProvider } from "@/design-system/components/editor/hooks/use-editor-context";
 import { WikiNode } from "@/builder/lexical-wiki-node";
+import { useDebouncer } from "@tanstack/react-pacer/debouncer";
+import { useNavigate } from "@tanstack/react-router";
 
 export const WikiSearch = () => {
-  const [isOpen, open, close] = useWikiStore((state) => [
+  const [wikiKey, isOpen, open, close] = useWikiStore((state) => [
+    state.wikiData.wiki.key,
     state.isSearchOpen,
     state.openSearch,
     state.closeSearch,
   ]);
   const [searchValue, setSearchValue] = useState("");
-  const { searchResults, isLoading } = useWikiSearch(searchValue);
+  const { searchResults } = useWikiSearch(searchValue);
+  const navigate = useNavigate();
 
-  console.log(searchResults);
-
-  // TODO: debounce
+  const debouncer = useDebouncer(
+    (searchValue: string) => {
+      setSearchValue(searchValue);
+    },
+    { wait: 400 },
+  );
 
   return (
     <CommandDialog
@@ -36,42 +43,53 @@ export const WikiSearch = () => {
       <Command className="max-w-sm rounded-lg border" shouldFilter={false}>
         <CommandInput
           placeholder="Search..."
-          value={searchValue}
-          onValueChange={setSearchValue}
+          onValueChange={(value) => debouncer.maybeExecute(value)}
         />
         <CommandList>
           <CommandEmpty>No results found.</CommandEmpty>
-          <CommandGroup heading="Results">
-            {searchResults?.map((article) => (
-              <CommandItem key={article.key} className="w-full justify-between">
-                <div>
+          {searchResults?.length ? (
+            <CommandGroup heading="Results">
+              {searchResults.map((article) => (
+                <CommandItem
+                  key={article.key}
+                  className="w-full justify-between"
+                  onSelect={() => {
+                    close();
+                    navigate({
+                      to: "/wikis/$wikiKey/$articleKey",
+                      params: { articleKey: article.key, wikiKey },
+                    });
+                  }}
+                >
                   <div>
-                    {article.category && (
-                      <>
-                        <span style={{ color: article.category.color }}>
-                          {article.category.name}
-                        </span>
-                        <span>&nbsp;&gt;&nbsp;</span>
-                      </>
-                    )}
-                    <span className="font-semibold">{article.title}</span>
+                    <div>
+                      {article.category && (
+                        <>
+                          <span style={{ color: article.category.color }}>
+                            {article.category.name}
+                          </span>
+                          <span>&nbsp;&gt;&nbsp;</span>
+                        </>
+                      )}
+                      <span className="font-semibold">{article.title}</span>
+                    </div>
+                    <EditorContextProvider
+                      entityType="wiki-article"
+                      entityKey={article.key}
+                    >
+                      <RichText
+                        editable={false}
+                        initialState={article.content}
+                        editorNodes={[WikiNode]}
+                        textDisplayMode="summary"
+                        className="text-muted-foreground text-xs"
+                      />
+                    </EditorContextProvider>
                   </div>
-                  <EditorContextProvider
-                    entityType="wiki-article"
-                    entityKey={article.key}
-                  >
-                    <RichText
-                      editable={false}
-                      initialState={article.content}
-                      editorNodes={[WikiNode]}
-                      textDisplayMode="summary"
-                      className="text-muted-foreground text-xs"
-                    />
-                  </EditorContextProvider>
-                </div>
-              </CommandItem>
-            ))}
-          </CommandGroup>
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          ) : null}
         </CommandList>
       </Command>
     </CommandDialog>
