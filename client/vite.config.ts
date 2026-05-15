6db601b054ce7d7505e66f3308c3005b6b0819e4
francisvirginius <francisvirginius@gmail.com>
2026-05-15 10:34:12 +0200

    Fix bug for page wiki-home.tsx

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -50,5 +50,13 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    dedupe: [
+      "lexical",
+      "@lexical/react",
+      "@lexical/rich-text",
+      "@lexical/selection",
+      "@lexical/table",
+      "@lexical/utils",
+    ],
   },
 });
